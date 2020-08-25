29897979279fc6ea066c83f8664479369bcf5013
aris suryono <aris.dwi.suryono@gmail.com>
2020-08-25 09:57:27 +0700

    init commit

---
+++ src/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Eve</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">6 Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+      </div>
+
+      <Link to="/checkout" className="header__link">
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </Link>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+      <img
+        className="home__image"
+        src="https://images.unsplash.com/photo-1574722772633-e401c33eb317?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2854&q=80"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Home;
